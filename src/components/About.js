80f7b6e1aa44b4fb4bdc2b1e0374917154dab4e7
shaun-tuscano <70930108+shaun-tuscano@users.noreply.github.com>
2024-03-03 18:41:03 +0530

    Minor update

--- src/components/About.js
+++ src/components/About.js
@@ -3,54 +3,68 @@ import NavBar from "../common/navbar";
 import "./styles/about.css"
 
 function About() {
-  return (
-    <div className="about-section">
-        <NavBar active="about"/>
-		<div className="about-container">
-		  <div className="about-section">
-			<h2 className="section-title">Who am I?</h2>
-			<p className="section-description">
-			As a software developer, my passion lies in creating innovative and efficient solutions to complex problems. With a strong foundation in computer science and hands-on experience in software development, I strive to deliver high-quality, scalable, and user-friendly applications.
-			Throughout my career, I have honed my skills in various programming languages such as Java, Python, C++, and JavaScript. I am well-versed in modern frameworks and technologies like React and Angular, enabling me to build robust and responsive web applications.
-			</p>
-		  </div>
-	
-		  <div className="experience-section">
-			<h2 className="section-title">Work Experience</h2>
+	return (
+		<div className="about-section">
+			<NavBar active="about" />
+			<div className="about-container">
+				<div className="about-section">
+					<h2 className="section-title">About Me</h2>
+					<p className="section-description">
+						Dedicated and results-driven Frontend Software Developer with 2+ years of experience in designing and
+						developing innovative user interfaces for dynamic web applications. Proven expertise in translating product requirements into scalable, high-performance, and user-friendly frontend solutions.Actively seeking
+						a position that offers challenging projects, providing a platform to showcase my skills and continually
+						learn in a dynamic professional environment.
+					</p>
+				</div>
 
-			<div className="experience-card">
-			  <h3 className="experience-title">Front-end Developer - jio platforms limited</h3>
-              <p className="section-description">Sept 2022 - Present</p>
-			  <p className="experience-description">
-				<ul>
-				<li>Developing modern and responsive web applications using React.</li>
-				<li>Collaborating with cross-functional teams to deliver high-quality frontend solutions.</li>
-				<li>Implementing UI designs, user interactions, and dynamic data rendering.</li>
-				</ul>
-			  </p>
+				<div className="experience-section">
+					<h2 className="section-title">Work Experience</h2>
+
+					<div className="experience-card">
+						<h3 className="experience-title">Front-end Developer - Jio Platforms Limited</h3>
+						<p className="section-description">Feb 2022 - Present</p>
+						<p className="experience-description">
+							<ul>
+								<li> Actively contributed to the development of a modernized user interface for an existing product,
+									enhancing user experience and visual appeal.
+								</li>
+								<li> Implemented key features such as version control and analytics dashboard, contributing to the
+									overall functionality and data-driven decision-making capabilities of the system.
+								</li>
+								<li>Conducted routine system sanity checks to proactively identify and address potential issues, ensuring a stable and reliable product</li>
+								<li> Engaged in bug fixing and performed regular website maintenance, ensuring optimal performance
+									and user satisfaction.
+								</li>
+								<li> Successfully refactored existing codebase to enhance system performance and maintainability.</li>
+								<li>laborated with clients to understand their requirements and translated them into robust features, aligning product development with client needs.
+								</li>
+
+
+							</ul>
+						</p>
+					</div>
+					<div className="experience-card">
+						<h3 className="experience-title">QA Tester - Jio Platforms Limited</h3>
+						<p className="section-description">Oct 2021 - Jan 2022</p>
+						<p className="experience-description">
+							<ul>
+								<li>Understanding and analyzing test requirements.</li>
+								<li>Contributing to test plan documentation.</li>
+								<li>Executing all levels of testing (Beta testing, Smoke testing, Regression) and uploading bugs on DevOps tracking tool</li>
+								<li>Communicating and collaborating with a team of 20+ testers.</li>
+								<li>Participating in different types of test environments such as PC, Android, iOS.</li>
+							</ul>
+						</p>
+					</div>
+
+
+				</div>
 			</div>
-			<div className="experience-card">
-			  <h3 className="experience-title">QA Tester - jio platforms limited</h3>
-              <p className="section-description">Oct 2021 - Oct 2022</p>
-			  <p className="experience-description">
-				<ul>
-				<li>Understanding and analyzing test requirements.</li>
-				<li>Contributing to test plan documentation.</li>
-				<li>Executing all levels of testing (Beta testing, Smoke testing, Regression) and uploading bugs on DevOps tracking tool</li>
-				<li>Communicating and collaborating with a team of 20+ testers.</li>
-				<li>Participating in different types of test environments such as PC, Android, iOS.</li>
-				</ul>
-			  </p>
-			</div>
-	
-			
-		  </div>
+			<Footer />
+
+
 		</div>
-        <Footer/>
-	
-
-    </div>
-  );
+	);
 }
 
 export default About;
